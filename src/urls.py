29d322cf5c35889ea61bc8e442bebd892332698b
bukun <bukun@osgeo.cn>
2016-05-26 22:20:49 +0800

    mark.

--- src/urls.py
+++ src/urls.py
@@ -1,17 +1,15 @@
-
+# -*- coding:utf-8 -*-
 from torcms.core import router
-# from torcms.handlers.info_handler import InfoHandler as AppHandler
 
-from torcms.applite.handlers.label_hander import AppLabelHandler
+from torcms.handlers.infolabel_hander import InfoLabelHandler
 from torcms.handlers.redirect_handler import RedirectHandler
 
 from router import urls as info_urls
+
 urls = router.urls + info_urls + [
 
-
-
     # ("/map/label_list/(.*)", AppLabellistHandler, dict()),
-     ("/list_label/(.*)", AppLabelHandler, dict()),
+    ("/list_label/(.*)", InfoLabelHandler, dict()),
 
     ("/(.*)", RedirectHandler, dict()),
 ]
