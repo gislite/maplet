c1e8de09e219f688acbca7892c550b8fbeaa5805
bukun <bukun@osgeo.cn>
2016-09-17 23:48:43 +0800

    mark.

--- static/jslib/gscript.js
+++ static/jslib/gscript.js
@@ -1,14 +1,63 @@
 $.ready()
 {
-    $("#searchForm").validate({
-                rules: {
-                    searchheader: "required"
-                },
-                messages: {
-                    searchheader: "<span class='red'>Please enter keywords</span>"
+    // 提交修改密码的动作
+    function js_update_pass() {
+        // 模拟Form提交
+        $.ajax({
+            type: "POST",
+            url: "/user/reset-password",
+            data: "email=" + $("#user_email").val()                   ,
 
+            success: function (msg) {
+                alert('已经成功重置了密码！请检查电子邮箱！');
+                location.href = '/user/info';
+            },
+            error: function () {
+                alert('密码重置失败，请确认Email是否有效，并且两次重置时间大于1分钟！');
+            }
+        })
+    }
+
+    $("#form_reset").validate(
+        {
+            rules: {
+                user_email: {
+                    required: true,
+                    email: true
                 }
-            });
+
+            },
+            messages: {
+                user_email: {
+                    required: "<span class='red'>请输入正确电子邮箱</span>",
+                    email: "<span class='red'>请输入正确的电子邮箱</span>"
+                }
+            }
+        }
+    );
+
+
+    $('#sub_reset').click(function () {
+        alert('Hello');
+        if ($("#form_reset").valid()) {
+            // do some stuff
+            js_update_pass();
+        }
+        else {
+            alert('Error');
+            // just show validation errors, dont post
+        }
+    });
+
+    $("#searchForm").validate({
+        rules: {
+            searchheader: "required"
+        },
+        messages: {
+            searchheader: "<span class='red'>Please enter keywords</span>"
+
+        }
+    });
 
     $('#act_collect').click(function () {
         $.ajax({
@@ -30,8 +79,7 @@ $.ready()
     });
 
     function g_load_postcat(ii) {
-        if ($('#pcat' + ii.toString()).val() == 0)
-        {
+        if ($('#pcat' + ii.toString()).val() == 0) {
             $('#gcat' + ii.toString()).empty();
         }
         else {
@@ -58,8 +106,7 @@ $.ready()
     }
 
     function g_load_infocat(ii) {
-        if ($('#pcat' + ii.toString()).val() == 0)
-        {
+        if ($('#pcat' + ii.toString()).val() == 0) {
             $('#gcat' + ii.toString()).empty();
         }
         else {
