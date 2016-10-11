15d179971abc032c3b7e1d5efa466fee74ce381d
bukun <bukun@osgeo.cn>
2016-10-12 00:07:40 +0800

    mark.

--- router.py
+++ router.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 
 # Todo: shouldn't be here.
-from torcms.handlers.info2reply_handler import Info2ReplyHandler as App2ReplyHandler
+# from torcms.handlers.info2reply_handler import Info2ReplyHandler as App2ReplyHandler
 from extor.handlers.overlay_handler import MapOverlayHandler
 from extor.handlers.map_handler import MapHandler
 from extor.handlers.geojson import GeoJsonHandler
@@ -11,7 +11,7 @@ from extor.handlers.map_handler import InforRedirectHandler
 urls = [
     ('/info/(.*)', InforRedirectHandler, dict()),
     ('/map/overlay/(.*)', MapOverlayHandler, dict()),
-    ('/map/toreply/(.*)', App2ReplyHandler, dict()),
+#     ('/map/toreply/(.*)', App2ReplyHandler, dict()),
     ('/map/(.*)', MapHandler, dict()),
     ("/list_label/(.*)", InfoLabelHandler, dict()),
     ('/geojson/(.*)', GeoJsonHandler, dict()),
--- static/jslib/maplet/mapshow.js
+++ static/jslib/maplet/mapshow.js
@@ -198,6 +198,8 @@
       maxZoom: 18,
       attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, " + "<a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, " + "Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
       id: "mapbox.streets"
+      // id: "mapbox.satellite"
+      // id: "mapbox.streets-satellite"
     });
     nexrad.addTo(cities);
     osm.addTo(cities);
