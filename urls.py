de836a1e96a9b326ad03baa1f6638ce20e51371c
bukun <bukun@osgeo.cn>
2017-02-18 20:36:35 +0800

    mark.

--- urls.py
+++ urls.py
@@ -1,11 +1,10 @@
 # -*- coding:utf-8 -*-
+
 from torcms.core import router
-
-# from torcms.handlers.redirect_handler import RedirectHandler
-
+from torcms.handlers.static_handler import StaticHandler
 from router import urls as info_urls
 
 urls = info_urls + router.urls + [
 
-    # ("/(.*)", RedirectHandler, dict()),
+    ("/(.*)", StaticHandler, dict()),
 ]
