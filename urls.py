22b274ceb67d1312f19c021fe074bf1fffc27cfb
bukun <bukun@osgeo.cn>
2017-05-28 19:03:04 +0800

    mark.

--- urls.py
+++ urls.py
@@ -3,8 +3,9 @@
 from torcms.core import router
 from torcms.handlers.static_handler import StaticHandler
 from router import urls as info_urls
+from torcms_maplet.core.router import maplet_urls
 
-urls = info_urls + router.urls + [
+urls = info_urls + maplet_urls + router.urls + [
 
     ("/(.*)", StaticHandler, dict()),
 ]
